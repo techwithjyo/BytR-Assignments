102dd8ed02953f1029735fcd4b393f70d8f76417
techwithjyo <jmaschatak1@gmail.com>
2024-09-25 23:21:07 +0530

    BD 5.1 - CW1

---
+++ BD5.1/BD5.1-CW/models/track.model.js
@@ -0,0 +1,30 @@
+const { DataTypes, sequelize } = require('../lib/index');
+
+const Track = sequelize.define('track', {
+  name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  artist: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  album: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  genre: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  duration: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  release_year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+module.exports = Track;
---
+++ BD5.1/BD5.1-CW/server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const { sequelize } = require('./lib/index');
+const Track = require('./models/track.model');
+
+const app = express();
+const port = 3000;
+
+const dummyData = [
+  {
+    name: 'Raabta',
+    genre: 'Romantic',
+    release_year: 2012,
+    artist: 'Arijit Singh',
+    album: 'Agent Vinod',
+    duration: 4,
+  },
+  {
+    name: 'Naina Da Kya Kasoor',
+    genre: 'Pop',
+    release_year: 2018,
+    artist: 'Amit Trivedi',
+    album: 'Andhadhun',
+    duration: 3,
+  },
+  {
+    name: 'Ghoomar',
+    genre: 'Traditional',
+    release_year: 2018,
+    artist: 'Shreya Ghoshal',
+    album: 'Padmaavat',
+    duration: 3,
+  },
+  {
+    name: 'Bekhayali',
+    genre: 'Rock',
+    release_year: 2019,
+    artist: 'Sachet Tandon',
+    album: 'Kabir Singh',
+    duration: 6,
+  },
+  {
+    name: 'Hawa Banke',
+    genre: 'Romantic',
+    release_year: 2019,
+    artist: 'Darshan Raval',
+    album: 'Hawa Banke (Single)',
+    duration: 3,
+  },
+  {
+    name: 'Ghungroo',
+    genre: 'Dance',
+    release_year: 2019,
+    artist: 'Arijit Singh',
+    album: 'War',
+    duration: 5,
+  },
+  {
+    name: 'Makhna',
+    genre: 'Hip-Hop',
+    release_year: 2019,
+    artist: 'Tanishk Bagchi',
+    album: 'Drive',
+    duration: 3,
+  },
+  {
+    name: 'Tera Ban Jaunga',
+    genre: 'Romantic',
+    release_year: 2019,
+    artist: 'Tulsi Kumar',
+    album: 'Kabir Singh',
+    duration: 3,
+  },
+  {
+    name: 'First Class',
+    genre: 'Dance',
+    release_year: 2019,
+    artist: 'Arijit Singh',
+    album: 'Kalank',
+    duration: 4,
+  },
+  {
+    name: 'Kalank Title Track',
+    genre: 'Romantic',
+    release_year: 2019,
+    artist: 'Arijit Singh',
+    album: 'Kalank',
+    duration: 5,
+  },
+];
+
+app.get('/seed_db', async (req, res) => {
+  try {
+    await sequelize.sync({ force: true }); // This will drop the table if it already exists
+    await Track.bulkCreate(dummyData);
+    res.status(200).send('Database seeded successfully');
+  } catch (error) {
+    console.error('Error seeding database:', error);
+    res.status(500).send('Error seeding database');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
